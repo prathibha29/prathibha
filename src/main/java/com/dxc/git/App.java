ab2662e8cd610874a301edecc262aba1abbafa54
prathibha <prathibha-shankar-s.prathibha@dxc.com>
2019-05-20 16:03:30 +0530

    First Commit

---
+++ src/main/java/com/dxc/git/App.java
@@ -0,0 +1,13 @@
+package com.dxc.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
